199162482df5700bc9ddb7c5e2527f9a74f15069
dongdong8343 <fkdlem8343@naver.com>
2025-10-20 21:01:46 +0900

    first commit

---
+++ src/main/java/io/github/dongdong8343/skinroutine/skinroutine/SkinroutineApplication.java
@@ -0,0 +1,13 @@
+package io.github.dongdong8343.skinroutine.skinroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinroutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinroutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dongdong8343/skinroutine/skinroutine/SkinroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dongdong8343.skinroutine.skinroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinroutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
